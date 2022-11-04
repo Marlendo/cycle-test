7b028e8c82c69f65aa0db74bb3e13ad8de1ff549
erik69-S <erikmarlendo@gmail.com>
2022-11-05 06:23:36 +0700

    hide morgann

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const helmet = require("helmet");
-const morgan = require('morgan');
-const path = require('path')
-const rfs = require('rotating-file-stream') // version 2.x
+// const morgan = require('morgan');
+// const path = require('path')
+// const rfs = require('rotating-file-stream') // version 2.x
 require('dotenv').config();
 const routesV1 = require('./routes/route.v1');
 
@@ -15,13 +15,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 // create a rotating write stream
-var accessLogStream = rfs.createStream('access.log', {
-  interval: '1d', // rotate daily
-  path: path.join(__dirname, 'log')
-});
+// var accessLogStream = rfs.createStream('access.log', {
+//   interval: '1d', // rotate daily
+//   path: path.join(__dirname, 'log')
+// });
 
 // setup the logger
-app.use(morgan('combined', { stream: accessLogStream }));
+// app.use(morgan('combined', { stream: accessLogStream }));
 
 /* app.use('/patients', routes.makes);
 app.use('/users', routes.users); */
