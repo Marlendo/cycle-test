901ccdf23da9b11be609df3c396bd362f690c1d3
erik69-S <erikmarlendo@gmail.com>
2022-11-04 09:22:19 +0700

    clear project

--- src/models/index.js
+++
@@ -1,5 +0,0 @@
-module.exports.Token = require('./token.model');
-module.exports.User = require('./user.model');
-module.exports.Promo = require('./promo.model');
-module.exports.Clinic = require('./clinic.model');
-module.exports.Blog = require('./blog.model');
--- src/services/index.js
+++
@@ -1,6 +0,0 @@
-module.exports.authService = require('./auth.service');
-module.exports.emailService = require('./email.service');
-module.exports.tokenService = require('./token.service');
-module.exports.userService = require('./user.service');
-module.exports.promoService = require('./promo.service');
-module.exports.clinicService = require('./clinic.service');
